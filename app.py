9406fd80782f0ad60bf8422e096c162e05eb42d0
nekantar-su <54645101+nekantar-su@users.noreply.github.com>
2022-04-19 15:58:55 -0400

    Testing

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 from twilio import twiml
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Worldfd!"
+    return "Hello World!"
 
-@app.route("/sms", methods=['GET', 'POST'])
+@app.route("/sms", methods=['POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Start our TwiML response
-    resp = twiml.MessagingResponse()
+    number = request.form['From']
+    message_body = request.form['Body']
+    resp = MessagingResponse()
     # Add a message
     resp.message("The Robots are coming! Head for the hills!")
 
