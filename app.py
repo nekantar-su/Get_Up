fde4401a87fde7125f5df4b90a84ba8b198a04b2
nekantar-su <54645101+nekantar-su@users.noreply.github.com>
2022-04-21 08:27:35 -0400

    Test

--- app.py
+++ app.py
@@ -20,13 +20,15 @@ def send_message(quotes_list = quotes):
     print("started")
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    twilio_number = os.environ['TWILIO_NUMBER']
+    to_number = os.environ['TO_NUMBER']
     client = Client(account_sid, auth_token)
 
     quote = random.choice(quotes_list)
     print(quote)
 
-    client.messages.create(from_='+17278557240',
-                            to='+19179601965',
+    client.messages.create(from_= twilio_number,
+                            to= to_number,
                            body=f" Daily Reminder to Be Great: \n {quote['text']} \n By: {quote['author']} \n Great Day To Have A Day! - Niko"
                            )
     print("reached")
