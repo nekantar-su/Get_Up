66a66897d69c8daa9ca2c3a8bc91757cc1306436
nekantar-su <54645101+nekantar-su@users.noreply.github.com>
2022-04-25 11:55:29 -0400

    db work to Heroku

--- app.py
+++ app.py
@@ -107,12 +107,13 @@ def sms_reply():
         resp.message(output)
     
     elif 'completed' in incoming_msg:
-        try:
-            task_id= incoming_msg.split('-')[1:]
-            print(task_id)
-            resp.message(deleteTask(number,int(task_id)))
-        except IndexError:
-            resp.message("Please enter in correct format. IE: Completed-Take out garbage")
+        resp.message('made it')
+        #try:
+        #    task_id= incoming_msg.split('-')[1:]
+        #    print(task_id)
+        #    resp.message(deleteTask(number,int(task_id)))
+        #except IndexError:
+        #    resp.message("Please enter in correct format. IE: Completed-Take out garbage")
 
     elif 'weather' in incoming_msg:
         weather_key=os.environ['WEATHER_KEY']
